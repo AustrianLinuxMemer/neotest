d873189d226bb860baee3e0200fe77a03d158b21
AustrianLinuxMemer <leo.fuereder@outlook.com>
2025-03-13 12:50:37 +0100

    improved the script to generate white noise

--- utils/generate_white_noise_image.sh
+++ utils/generate_white_noise_image.sh
@@ -4,7 +4,12 @@ width=$2
 height=$3
 if [[ -z $image_path || -z $width ]]; then
     echo "Usage: $0 <image_path> width [height]"
+    exit 1
 else
+    if [[ ! $width =~ ^[0-9]+$ || ! $height =~ ^[0-9]+$ ]]; then
+        echo "width/height must be numbers"
+        exit 1
+    fi
     if [[ -z $height ]]; then
         head -c $(("$width"*"$width")) /dev/urandom | ffmpeg -f rawvideo -pixel_format gray -video_size "$width"x"$width" -i - -frames:v 1 "$image_path"
     else
