3dddd6b2ce2d3c73866a8f267245a772e90bcfbf
AustrianLinuxMemer (local) <leo.fuereder@outlook.com>
2025-04-03 14:24:29 +0200

    Furnace now works like expected

--- mods/furnace/init.lua
+++ mods/furnace/init.lua
@@ -4,6 +4,7 @@ local function init_furnace(pos)
     local inv = meta:get_inventory()
     inv:set_size("fuel", 1)
     inv:set_size("input", 1)
+    inv:set_size("temp", 1)
     inv:set_size("result", 1)
     meta:set_string("formspec", furnace_ui)
 end
@@ -11,13 +12,21 @@ local function open_furnace(pos, node, player, itemstack, pointed_thing)
     local meta = minetest.get_meta(pos)
     core.show_formspec(player:get_player_name(), "furnace:furnace", meta:get_string("formspec"))
 end
+local function set_furnace_active(pos, on)
+    local furnace_type = core.get_node(pos).name
+    if on and furnace_type == "furnace:furnace" then
+        core.swap_node(pos, {name = "furnace:active_furnace"})    
+    elseif not on and furnace_type == "furnace:active_furnace" then
+        core.swap_node(pos, {name = "furnace:furnace"})
+    end
+end
 local function furnace_loop(pos, node, active_object_count, active_object_count_wider)
     local meta = core.get_meta(pos)
     local inv = meta:get_inventory()
     local fuel_stack = inv:get_stack("fuel", 1)
     local input_stack = inv:get_stack("input", 1)
     local output_stack = inv:get_stack("result", 1)
-    local furnace_type = core.get_node(pos).name
+    local temp_stack = inv:get_stack("temp", 1)
     local output = core.get_craft_result({
         method = "cooking",
         width = 1,
@@ -28,45 +37,43 @@ local function furnace_loop(pos, node, active_object_count, active_object_count_
         width = 1,
         items = {fuel_stack}        
     })
+    
     local remaining_fuel = meta:get_int("remaining_fuel")
-    local remaining_input = meta:get_int("remaining_input")
-    -- Checking if the fuel is empty
+    local remaining_item = meta:get_int("remaining_item")
     if remaining_fuel > 0 then
-        -- Setting the furnace alight
-        core.swap_node(pos, {name = "furnace:active_furnace"})
-        -- Consuming the fuel
         meta:set_int("remaining_fuel", remaining_fuel - 1)
-        if remaining_input > 0 then
-            -- Cooking the item
-            meta:set_int("remaining_input", remaining_input - 1)        
+        set_furnace_active(pos, true)
+        if remaining_item > 0 then
+            meta:set_int("remaining_item", remaining_item - 1)
         else
+            -- Empty out the temp stack
+            if not temp_stack:is_empty() then
+                output_stack:add_item(temp_stack)
+                temp_stack:clear()            
+            end
+            -- Putting in the next item
             if not input_stack:is_empty() and not output.item:is_empty() then
                 input_stack:take_item(1)
-                meta:set_int("remaining_input", output.time)            
+                temp_stack:add_item(output.item)
+                meta:set_int("remaining_item", output.time)
             end
-            output_stack:add_item(output.item)
-
         end
     else
-        
-        
-        -- Replenishing the Fuel
-        if not fuel_stack:is_empty() then
-            meta:set_int("remaining_fuel", fuel.time)
+        if not fuel_stack:is_empty() and fuel.time ~= 0 and not input_stack:is_empty() then
             fuel_stack:take_item(1)
+            meta:set_int("remaining_fuel", fuel.time)
+            set_furnace_active(pos, true)
         else
-            -- Extinguishing the furnace
-            core.swap_node(pos, {name = "furnace:furnace"})
+            set_furnace_active(pos, false)
+            meta:set_int("remaining_item", 0)
+            temp_stack:clear()
         end
-        
     end
-    -- Updating item stacks
+    
     inv:set_stack("fuel", 1, fuel_stack)
     inv:set_stack("input", 1, input_stack)
     inv:set_stack("result", 1, output_stack)
-    core.chat_send_all("Fuel: "..tostring(remaining_fuel))
-    core.chat_send_all("Input: "..tostring(remaining_input))
-    core.chat_send_all("Recipe to cook: "..input_stack:get_name().." -> "..output.item:get_name().." ("..output.item:get_count()..")")
+    inv:set_stack("temp", 1, temp_stack)
 end
 
 core.register_node("furnace:furnace", {
