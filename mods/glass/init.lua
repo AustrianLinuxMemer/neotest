0ef7744c061ec33ad04bc571c298a7e93c326b96
AustrianLinuxMemer (local) <leo.fuereder@outlook.com>
2025-04-06 02:48:00 +0200

    Made changes to glass panes

--- mods/glass/init.lua
+++ mods/glass/init.lua
@@ -7,7 +7,16 @@ local glass_def = {
     paramtype = "light",
     groups = {choppy = 2, glass = 1}
 }
+local glass_pane_def = {
+    tiles = {"glass_pane_side.png","glass_pane_side.png","glass_glass.png","glass_glass.png","glass_glass.png","glass_glass.png"},
+    use_texture_alpha = "blend",
+    sunlight_propagates = true,
+    paramtype = "light",
+    groups = {choppy = 2, glass = 1}
+}
 core.register_node("glass:glass", glass_def)
+panes.register_pane("glass:glass", "Glass Pane", "glass_pane_front.png", "glass_glass.png",glass_pane_def)
+
 stairs.register_stair("glass:glass", glass_def.description.." Stairs", glass_def, true)
 stairs.register_slab("glass:glass", glass_def.description.." Slab", glass_def, true)
 core.register_craft({
